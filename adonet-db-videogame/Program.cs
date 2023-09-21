ff2886a4815cfdcdab2487264b3aa71941a3bc37
leonardoserra <serra.leonardo92@gmail.com>
2023-09-21 15:02:47 +0200

    case 5 completo// chiudere programma

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -5,7 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("------------------------Gestionale Videogames per i Tornei------------------------");
-            Console.WriteLine(@"Seleziona l'operazione da effettuare: 
+            bool programExecuting = true;
+            while (programExecuting)
+            {
+                Console.WriteLine(@"Seleziona l'operazione da effettuare: 
 
             1. Inserire un nuovo videogioco
             2. Cerca per id 
@@ -14,6 +17,37 @@
             5. Chiudere il programma
 
             ");
+
+                int selectedOperation = int.Parse(Console.ReadLine());
+                switch (selectedOperation)
+                {
+                    case 1:
+                        //Inserire un nuovo videogioco
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        programExecuting = false;
+                        Console.WriteLine("Chiusura programma in corso...");
+                        Thread.Sleep(1000);
+                        Console.WriteLine("......");
+                        Thread.Sleep(500);
+                        Console.WriteLine("....");
+                        Thread.Sleep(555);
+                        Console.WriteLine("Alla prossima! ;)");
+                        Thread.Sleep(1000);
+
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        break;
+                }
+            }
         }
     }
 }
