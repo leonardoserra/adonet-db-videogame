0f7055394edc2c16b4df7e435b1f661bd1961b40
leonardoserra <serra.leonardo92@gmail.com>
2023-09-21 15:11:52 +0200

    caso default ok funzione per "animazione" di uscita ExitAnimation()

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -26,28 +26,42 @@
 
                         break;
                     case 2:
+                        //Cerca per id
                         break;
                     case 3:
+                        //Cerca per nome (anche parziale)
                         break;
                     case 4:
+                        //Cancellare un videogioco
                         break;
                     case 5:
+                        //Chiudere il programma
                         programExecuting = false;
-                        Console.WriteLine("Chiusura programma in corso...");
-                        Thread.Sleep(1000);
-                        Console.WriteLine("......");
-                        Thread.Sleep(500);
-                        Console.WriteLine("....");
-                        Thread.Sleep(555);
-                        Console.WriteLine("Alla prossima! ;)");
-                        Thread.Sleep(1000);
-
+                        ExitAnimation();
                         break;
                     default:
-                        Console.WriteLine("");
+                        Console.WriteLine("Comando non valido");
+                        Console.WriteLine("Vuoi uscire? y/n");
+                        string answer = Console.ReadLine().ToLower();
+                        if(answer == "y" || answer == "yes" || answer == "s" || answer == "si")
+                        {
+                            programExecuting = false;
+                            ExitAnimation();
+                        }
                         break;
                 }
             }
+        }
+        public static void ExitAnimation()
+        {
+            Console.WriteLine("Chiusura programma in corso...");
+            Thread.Sleep(1000);
+            Console.WriteLine("......");
+            Thread.Sleep(500);
+            Console.WriteLine("....");
+            Thread.Sleep(555);
+            Console.WriteLine("Alla prossima! ;)");
+            Thread.Sleep(1000);
         }
     }
 }
